94e2d6ebb7f28155ebfe47b2dcc5ab081b795ef0
Sergey <piankov5206@gmail.com>
2016-02-18 17:19:22 +0300

    Change steps describing

--- tickets/test_tickets_1.js
+++ tickets/test_tickets_1.js
@@ -4,10 +4,9 @@ var webdriverio = require('webdriverio');
 
 
 const BUG1 = true; //Logo always leads to Russian page
-/*
-Discription:
-    Check different links to the tickets page */
+
 var steps = [
+	"Discription:\tCheck different links to the tickets page",
     "1. Open 'Что такое Кидзания?' page (Russian) (https://kidzania.ru/ru/about/)",
     "2. Click on 'Билеты' link",
     "3. Check russian ticket page is opened (https://kidzania.ru/ru/tickets/)",
@@ -35,7 +34,8 @@ console.log(steps[0]);
 var client = webdriverio
     .remote(options)
 	.init();
-
+	
+console.log(steps[1]);
 	
 client.addCommand("getUrlAndTitle", function() {
     return this.getUrl().then(function(urlResult) {
@@ -45,55 +45,54 @@ client.addCommand("getUrlAndTitle", function() {
     });
 });
 
-
 var currentURL = TicketsPage.mainURL+language+'about/'
 client.url(currentURL)
     .getUrlAndTitle()
 		.then(function(result){
 			TicketsPage.assertUrlTitle(result, currentURL, TicketsPage.aboutTitleR);
-			console.log(steps[1]);
+			console.log(steps[2]);
 		})
 	.click('=Билеты')
 	.getUrlAndTitle()
 		.then(function(result){
-			console.log(steps[2]);
+			console.log(steps[3]);
 			currentURL = TicketsPage.mainURL+language+'tickets/';
 			TicketsPage.assertUrlTitle(result, currentURL, TicketsPage.ticketsTitleR);
-			console.log(steps[3]);
+			console.log(steps[4]);
 		})
 	.click('=Кидзания в мире')
 	.getUrlAndTitle()
 		.then(function(result){
-			console.log(steps[4]);
+			console.log(steps[5]);
 			currentURL = TicketsPage.mainURL+language+'world/';
 			TicketsPage.assertUrlTitle(result, currentURL, TicketsPage.worldTitleR);
-			console.log(steps[5])
+			console.log(steps[6])
 		})
 	//Click to open menu with languages
 	.click(TicketsPage.languageHeader)
 	.waitForVisible('=English', 3000)
 	.click('=English')
 		.then(function(result){
-			console.log(steps[6])
+			console.log(steps[7])
 			language = "en/"
 		})
 	.click('=Tickets')
 	.getUrlAndTitle()
 		.then(function(result){
-			console.log(steps[7])
+			console.log(steps[8])
 			currentURL = TicketsPage.mainURL+language+'tickets/';
 			TicketsPage.assertUrlTitle(result, currentURL, TicketsPage.ticketsTitleE);
-			console.log(steps[8])
+			console.log(steps[9])
 		})
 	.click(TicketsPage.logoHeader)
 		.then(function(result){
 			if (BUG1){
 				console.log('Clicking logo causes changing language, Step 10 skiped');
 				language = "ru/";
-				console.log(steps[10])
+				console.log(steps[11])
 			}
 			else{
-				console.log(steps[9])
+				console.log(steps[10])
 			}
 			
 		})
@@ -102,14 +101,14 @@ client.url(currentURL)
 	.waitForVisible('=Русский', 3000)
 	.click('=Русский')
 		.then(function(result){
-			console.log(steps[10])
+			console.log(steps[11])
 			language = "ru/"
 		})
 		*/
 	.click('=Купить билет')
 	.getUrlAndTitle()
 		.then(function(result){
-			TicketsPage.printStep(12);
+			console.log(steps[12])
 			currentURL = TicketsPage.mainURL+language+'tickets/';
 			TicketsPage.assertUrlTitle(result, currentURL, TicketsPage.ticketsTitleR);
 		})	
