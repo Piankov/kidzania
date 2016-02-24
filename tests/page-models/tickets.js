2cd246f3e0ab6f1c44906cb809db1cff9f97330c
Sergey <piankov5206@gmail.com>
2016-02-25 00:46:43 +0300

    Add several tests for time slot and start adding multylanguage

--- tests/page-models/tickets.js
+++ tests/page-models/tickets.js
@@ -3,7 +3,15 @@ var assert = require('assert')
 
 module.exports = {
 	getFormatedDate: function(date){
-		return date.match(/-(\d\d)$/)[1] + ' ' + this.month[parseInt(date.match(/-(\d\d)-/)[1].replace(/^0/,""))-1]
+		var day = date.match(/-(\d\d)$/)[1].replace(/^0/,"");
+		if (this.language == 0) {
+			day = '1';
+			if (day == 1) day += 'st';
+			if (day == 2) day += 'nd';
+			if (day == 3) day += 'rd';
+			if (parseInt(day) > 3) day += 'th';
+		}
+		return day + ' ' + this.month[this.language][parseInt(date.match(/-(\d\d)-/)[1].replace(/^0/,""))-1];
 	},
 	languageHeader: ".header__lang",
 	logoHeader: ".header__logo",
@@ -13,6 +21,13 @@ module.exports = {
 	totals: ".tickets-totals__fixed",
 	dateTotals: ".tickets-date__format",
 	closeDateTotals: ".tickets-date__close",
-	month: ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"]
-	
+	slotTotals: ".tickets-slot__format",
+	closeSlotTotals: ".tickets-slot__close",
+	month: [['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+	["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"]],
+	language: 1 //0 - English, 1 - Russian
+	//langLink: function(){
+	//	return this.langLinkArray[this.language];
+	//},
+	//langLinkArray: ['en/', 'ru/']
 };
---
+++ tests/tickets/test_tickets_3.js
@@ -0,0 +1,137 @@
+var TicketsPage = require('../page-models/tickets');
+var should = require('should');
+
+describe('Check clicking dates', function(){
+	this.timeout(15000);
+
+	browser.addCommand("getTotalDate", function() {
+		return this.waitForExist(TicketsPage.dateTotals)
+				.element(TicketsPage.dateTotals)
+					.then(function(result) {
+						return browser.elementIdText(result.value.ELEMENT);
+					})
+						//Why I can't just add .value to previous string?
+						.then(function(result){
+							return result.value;
+						});
+	});
+	browser.addCommand("getElementDate", function(selector) {
+		return this.element(selector)
+					.then(function(result) {
+						return browser.elementIdAttribute(result.value.ELEMENT,'data-date-key')
+					})
+						.then(function(result){
+							return TicketsPage.getFormatedDate(result.value);
+						});
+	});
+	browser.addCommand("getTotalSlot", function() {
+		return this.waitForExist(TicketsPage.slotTotals)
+				.element(TicketsPage.slotTotals)
+					.then(function(result) {
+						return browser.elementIdText(result.value.ELEMENT);
+					})
+						//Why I can't just add .value to previous string?
+						.then(function(result){
+							return result.value;
+						});
+	});
+	browser.addCommand("getElementSlot", function(selector) {
+		return this.element(selector)
+					.then(function(result) {
+						return browser.elementIdText(result.value.ELEMENT)
+					})
+						.then(function(result){
+							return result.value;
+						});
+	});
+
+	it('should appear totals', function () {
+		// Click on any date
+		// Click on any slot
+		// Check futter appeared and slot is correct
+		this.skip();
+		var slot;
+		return browser.url('https://kidzania.ru/ru/tickets')
+			.click('.calendar-item.high:nth-Child(7)')
+			.scroll('.tickets-slots__item.high')
+			.getElementSlot('.tickets-slots__item.high:nth-Child(2)')
+				.then(function(result){
+					slot = result;
+				})
+			.click('.tickets-slots__item.high:nth-Child(2)')
+			.getTotalSlot()
+				.then(function(result){
+					result.should.be.equal(slot);
+				});
+
+	})
+	it('should change slot in totals', function () {
+		// Click on any date
+		// Click on any slot
+		// Click on any other slot
+		// Check slot in footer has changed
+		this.skip();
+		var slot;
+		return browser.url('https://kidzania.ru/ru/tickets')
+			.click('.calendar-item.high:nth-Child(15)')
+			.scroll('.tickets-slots__item.high')
+			.getElementSlot('.tickets-slots__item.high:nth-Child(4)')
+				.then(function(result){
+					slot = result;
+				})
+			.click('.tickets-slots__item.high:nth-Child(1)')
+			.click('.tickets-slots__item.high:nth-Child(4)')
+			.getTotalSlot()
+				.then(function(result){
+					result.should.be.equal(slot);
+				});
+
+	})
+
+	it('should save slot when date is canceled', function () {
+		// Click on any date
+		// Click on any slot
+		// Click on X near the date in footer
+		// Check "Выберите дату" appeared (RU) and slot hasn't been changed
+		// Click on any date (which contains this slot)
+		// Date appeared and slot hasn't been changed
+		//this.skip();
+		var slot;
+		var date;
+		return browser.url('https://kidzania.ru/ru/tickets')
+			.click('.calendar-item.high:nth-Child(9)')
+			.scroll('.tickets-slots__item.high')
+			.getElementSlot('.tickets-slots__item.high:nth-Child(2)')
+				.then(function(result){
+					slot = result;
+				})
+			.click('.tickets-slots__item.high:nth-Child(2)')
+			.click(TicketsPage.closeDateTotals)
+			.getTotalSlot()
+				.then(function(result){
+					result.should.be.equal(slot);
+				})
+			.element('.tickets-dateslot-group .tickets-totals__blank')
+				.then(function(result) {
+						return browser.elementIdText(result.value.ELEMENT);
+					})
+						.then(function(result){
+							result.value.should.be.equal('Выберите дату');
+						})
+			.getElementDate('.calendar-item.high:nth-Child(11)')
+				.then(function(result){
+					date = result;
+				})
+			.click('.calendar-item.high:nth-Child(11)')
+			.getTotalDate()
+				.then(function(result){
+					result.should.be.equal(date);
+				})
+			.getTotalSlot()
+				.then(function(result){
+					result.should.be.equal(slot);
+				});
+	})
+
+	
+})
