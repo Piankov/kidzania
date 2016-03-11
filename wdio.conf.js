657db832a94098b5ea8b2f9dd1be270e5023cbdb
Sergey <piankov5206@gmail.com>
2016-03-11 12:49:24 +0300

    Trying to remove problem with connection to selenium on Travis

--- wdio.conf.js
+++ wdio.conf.js
@@ -12,6 +12,8 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
+    host: 'localhost',
+    port: 4444,
     specs: [
         './tests/tickets/*.js'
     ],
